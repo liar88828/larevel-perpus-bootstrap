34defd7108309c94371f841fa0895f1a80832eb0
prasojowahyu <wahyuprasojo@students.unnes.ac.id>
2023-07-11 20:52:05 +0700

    save migrate to mysql

---
+++ app/Http/Controllers/ModelIjinLemburController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ModelIjinLembur;
+use App\Http\Requests\StoreModelIjinLemburRequest;
+use App\Http\Requests\UpdateModelIjinLemburRequest;
+
+class ModelIjinLemburController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreModelIjinLemburRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ModelIjinLembur $modelIjinLembur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ModelIjinLembur $modelIjinLembur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateModelIjinLemburRequest $request, ModelIjinLembur $modelIjinLembur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ModelIjinLembur $modelIjinLembur)
+    {
+        //
+    }
+}
